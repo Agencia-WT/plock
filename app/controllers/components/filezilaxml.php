04522c91ef125e72c2c92805f5e0ef29bdfee7d7
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2011-11-22 16:04:17 -0200

    BugFix: Corrigido bug que importava apenas um dominio do cliente

--- app/controllers/components/filezilaxml.php
+++ app/controllers/components/filezilaxml.php
@@ -12,26 +12,33 @@ class FilezilaxmlComponent extends Object
 			$a .= '<Servers>';
 			foreach($clientes as $c){
 				
-				$a .= '<Server>';
-					$a .= '<Host>'.( isset($c['Dominio'][0]['ftp_host']) ? $c['Dominio'][0]['ftp_host'] : '' ).'</Host>';
-					$a .= '<Port>21</Port>';
-					$a .= '<Protocol>0</Protocol>';
-		            $a .= '<Type>0</Type>';
-		            $a .= '<User>'.( isset($c['Dominio'][0]['ftp_username']) ? $c['Dominio'][0]['ftp_username'] : '' ).'</User>';
-		            $a .= '<Pass>'.( isset($c['Dominio'][0]['ftp_password']) ? $c['Dominio'][0]['ftp_password'] : '' ).'</Pass>';
-		            $a .= '<Logontype>1</Logontype>';
-		            $a .= '<TimezoneOffset>0</TimezoneOffset>';
-		            $a .= '<PasvMode>MODE_DEFAULT</PasvMode>';
-		            $a .= '<MaximumMultipleConnections>0</MaximumMultipleConnections>';
-		            $a .= '<EncodingType>Auto</EncodingType>';
-		            $a .= '<BypassProxy>0</BypassProxy>';
-		            $a .= '<Name>'.$c['Cliente']['nome'].'</Name>';
-		            $a .= '<Comments />';
-		            $a .= '<LocalDir />';
-		            $a .= '<RemoteDir />';
-		            $a .= '<SyncBrowsing>0</SyncBrowsing>'.$c['Cliente']['nome'].'#x0A';
-				$a .= '</Server>';
+				# Foreach para cada dominio do cliente
+				foreach($c['Dominio'] as $d){
+					
+					$a .= '<Server>';
+						$a .= '<Host>'.( isset($d['ftp_host']) ? $d['ftp_host'] : '' ).'</Host>';
+						$a .= '<Port>21</Port>';
+						$a .= '<Protocol>0</Protocol>';
+			            $a .= '<Type>0</Type>';
+			            $a .= '<User>'.( isset($d['ftp_username']) ? $d['ftp_username'] : '' ).'</User>';
+			            $a .= '<Pass>'.( isset($d['ftp_password']) ? $d['ftp_password'] : '' ).'</Pass>';
+			            $a .= '<Logontype>1</Logontype>';
+			            $a .= '<TimezoneOffset>0</TimezoneOffset>';
+			            $a .= '<PasvMode>MODE_DEFAULT</PasvMode>';
+			            $a .= '<MaximumMultipleConnections>0</MaximumMultipleConnections>';
+			            $a .= '<EncodingType>Auto</EncodingType>';
+			            $a .= '<BypassProxy>0</BypassProxy>';
+			            $a .= '<Name>'.$c['Cliente']['nome'].'</Name>';
+			            $a .= '<Comments />';
+			            $a .= '<LocalDir />';
+			            $a .= '<RemoteDir />';
+			            $a .= '<SyncBrowsing>0</SyncBrowsing>'.$c['Cliente']['nome'].'#x0A';
+					$a .= '</Server>';					
+				}
+				
+
 			}
+			
 			$a .= '</Servers>';
 		$a .= '</FileZilla3>';
 	
